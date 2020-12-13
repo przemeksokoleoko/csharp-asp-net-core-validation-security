6dd3f71e80ce3bb2c810822de6276e5a38dbb366
przemeksokoleoko <przemeksokoleoko@outlook.com>
2020-12-13 21:04:20 +0100

    Improve Security Using Validation

--- ConferenceTracker/Controllers/SpeakersController.cs
+++ ConferenceTracker/Controllers/SpeakersController.cs
@@ -46,10 +46,18 @@ namespace ConferenceTracker.Controllers
 
         [HttpPost]
         [Authorize(Roles = "Administrators")]
-        public IActionResult Create(Speaker speaker)
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id,FirstName,LastName,Description,EmailAddress,PhoneNumber")] Speaker speaker)
         {
-            _speakerRepository.Create(speaker);
-            return RedirectToAction(nameof(Index));
+            if (ModelState.IsValid)
+            {
+                _speakerRepository.Create(speaker);
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(speaker);
+            }
         }
 
         [HttpGet]
--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -4,18 +4,41 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ConferenceTracker.Entities
 {
-    public class Speaker
+    public class Speaker : IValidatableObject
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(maximumLength: 100, MinimumLength = 2)]
         [Display(Name = "First name")]
         public string FirstName { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(maximumLength: 100, MinimumLength = 2)]
         [Display(Name = "Last name")]
         public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [StringLength(maximumLength: 500, MinimumLength = 10)]
         public string Description { get; set; }
+        [DataType(DataType.EmailAddress)]
         [Display(Name = "Email Address")]
         public string EmailAddress { get; set; }
+        [DataType(DataType.PhoneNumber)]
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
         public bool IsStaff { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var validationResult = new List<ValidationResult>();
+
+            if (EmailAddress?.EndsWith("TechnologyLiveConference.com", StringComparison.InvariantCultureIgnoreCase) == true)
+            {
+                validationResult.Add(new ValidationResult("Technology Live Conference staff should not use their conference email addresses."));
+            }
+            return validationResult;
+        }
     }
 }
